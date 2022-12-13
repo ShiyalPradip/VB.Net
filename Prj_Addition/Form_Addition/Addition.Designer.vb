b083ffbbc2856bf720dac7dbc276d0783875ec76
Shiyal Pradip ✌ <98959751+ShiyalPradip@users.noreply.github.com>
2022-12-14 05:05:56 +0530

    Add files via upload

---
+++ Prj_Addition/Form_Addition/Addition.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Addition
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.TxtNum1 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.TxtNum2 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.TxtNum3 = New System.Windows.Forms.TextBox
+        Me.Button = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(117, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Addition Of Two Numbers"
+        '
+        'TxtNum1
+        '
+        Me.TxtNum1.Location = New System.Drawing.Point(150, 53)
+        Me.TxtNum1.Name = "TxtNum1"
+        Me.TxtNum1.Size = New System.Drawing.Size(120, 20)
+        Me.TxtNum1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Enter a Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Enter a Number"
+        '
+        'TxtNum2
+        '
+        Me.TxtNum2.Location = New System.Drawing.Point(150, 94)
+        Me.TxtNum2.Name = "TxtNum2"
+        Me.TxtNum2.Size = New System.Drawing.Size(120, 20)
+        Me.TxtNum2.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(99, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Addition"
+        '
+        'TxtNum3
+        '
+        Me.TxtNum3.Location = New System.Drawing.Point(150, 133)
+        Me.TxtNum3.Name = "TxtNum3"
+        Me.TxtNum3.ReadOnly = True
+        Me.TxtNum3.Size = New System.Drawing.Size(120, 20)
+        Me.TxtNum3.TabIndex = 6
+        '
+        'Button
+        '
+        Me.Button.Location = New System.Drawing.Point(150, 185)
+        Me.Button.Name = "Button"
+        Me.Button.Size = New System.Drawing.Size(75, 23)
+        Me.Button.TabIndex = 7
+        Me.Button.Text = "Calculate"
+        Me.Button.UseVisualStyleBackColor = True
+        '
+        'Addition
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 224)
+        Me.Controls.Add(Me.Button)
+        Me.Controls.Add(Me.TxtNum3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtNum2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtNum1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Addition"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Addition"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TxtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TxtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TxtNum3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button As System.Windows.Forms.Button
+
+End Class
---
+++ Prj_Addition/Form_Addition/Addition.vb
@@ -0,0 +1,17 @@
+﻿Public Class Addition
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button.Click
+        TxtNum3.Text = Val(TxtNum1.Text) + Val(TxtNum2.Text)
+    End Sub
+
+    Private Sub Addition_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
